bf13a25a1b2cd48ca027db310c8ed05d30bd72b5
Jerome Jahnke <jahnke@tormenta.com>
2025-10-19 01:49:21 -0500

    fix: Clone repo in run_playbook.sh to ensure latest version

--- run_playbook.sh
+++ run_playbook.sh
@@ -20,7 +20,10 @@ echo "Running Ansible playbook against: $INVENTORY"
 # Disable host key checking for ephemeral machines
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-# Execute the playbook
-ansible-playbook -i "$INVENTORY" install_git.yml --user jahnke --vault-password-file vault_pass.txt
+# Clone the repository to a temporary directory
+git clone https://github.com/jjjahnke/chezmoi.git /tmp/chezmoi
+
+# Execute the playbook from the local clone
+ansible-playbook -i "$INVENTORY" /tmp/chezmoi/install_git.yml --user jahnke --vault-password-file /tmp/chezmoi/vault_pass.txt
 
 echo "Playbook execution finished."
